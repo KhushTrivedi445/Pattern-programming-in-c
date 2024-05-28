186bbd7a81c22005a4b5a8e438d48b046ebeb586
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-05-28 22:19:14 +0530

    Add files via upload

---
+++ program to print mirror image of right angled triangle/pattern.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main () {
+int n;
+printf("Enter the number of rows you want: ");
+scanf("%d",&n);
+
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=n;j++){
+        if (j>=6-i){
+        printf("*");
+        } else {
+        printf(" ");
+        }
+    }
+    printf("\n");
+}
+
+
+  return 0;
+}
